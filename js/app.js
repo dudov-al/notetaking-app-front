7f180aebf4222ad88eb9125dd406d5383d09fa61
dudov-al <31045292+dudov-al@users.noreply.github.com>
2020-06-27 20:24:15 +0200

    Init commit

---
+++ js/app.js
@@ -0,0 +1,113 @@
+function createElement(tag, props, ...children) {
+  const element = document.createElement(tag);
+
+  Object.keys(props).forEach((key) => (element[key] = props[key]));
+
+  if (children.length > 0){
+      children.forEach(child => {
+        if (typeof child === 'string'){
+            child = document.createTextNode(child);
+        }
+
+        element.appendChild(child);
+      });
+  }
+
+  return element;
+}
+
+function createTodoItem(title) {
+  const checkbox = createElement("input", {
+    type: "checkbox",
+    className: "checkbox",
+  }); //создаем элемнт input
+  // присваиваем класс для input
+
+  const label = createElement("label", { className: "title" }, title);
+
+  const editInput = createElement("input", {
+    type: "text",
+    className: "textfield",
+  });
+
+  const editButton = createElement("button", { className: "edit" }, "Edit");
+
+  const deleteButton = createElement(
+    "button",
+    { className: "delete" },
+    "Delete"
+  );
+
+  const listItem = createElement(
+    "li",
+    { className: "todo-item" },
+    checkbox,
+    label,
+    editInput,
+    editButton,
+    deleteButton
+  );
+
+  bindEvents(listItem);
+
+  return listItem;
+}
+
+function bindEvents(todoItem) {
+  const checkbox = todoItem.querySelector(".checkbox"); //подписываемся на событие
+  const editButton = todoItem.querySelector("button.edit");
+  const deleteButton = todoItem.querySelector("button.delete");
+
+  checkbox.addEventListener("change", toggleTodoItem);
+  editButton.addEventListener("click", editTodoItem);
+  deleteButton.addEventListener("click", deleteTodoItem);
+}
+
+function addTodoItem(event) {
+  event.preventDefault(); //Остановит отправку на сервер т.е. страница не перезагрузится
+
+  if (addInput.value === "") return alert("Add task, please");
+
+  const todoItem = createTodoItem(addInput.value);
+  todoList.appendChild(todoItem);
+  addInput.value = "";
+}
+
+function toggleTodoItem() {
+  const listItem = this.parentNode;
+  listItem.classList.toggle("completed");
+}
+
+function editTodoItem() {
+  const listItem = this.parentNode;
+  const title = listItem.querySelector(".title");
+  const editInput = listItem.querySelector(".textfield");
+  const isEditing = listItem.classList.contains("editing");
+
+  if (isEditing) {
+    title.innerText = editInput.value;
+    this.innerText = "Edit";
+  } else {
+    editInput.value = title.innerText;
+    this.innerText = "Save";
+  }
+
+  listItem.classList.toggle("editing");
+}
+
+function deleteTodoItem() {
+  const listItem = this.parentNode;
+  todoList.removeChild(listItem);
+}
+//Получение доступа к элементам DOM из HTML документа
+const todoForm = document.getElementById("todo-form");
+const addInput = document.getElementById("add-input");
+const todoList = document.getElementById("todo-list");
+const todoItems = document.querySelectorAll(".todo-item");
+
+function main() {
+  todoForm.addEventListener("submit", addTodoItem);
+  todoItems.forEach((item) => bindEvents(item));
+}
+
+main();
